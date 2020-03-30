a7ff6d31b7a73db2f21ebbfc739404b432b954fc
1v0dev <1v0dev@protonmail.com>
2020-03-30 14:42:29 +0300

    switch service to foreground and START_STICKY

--- app/src/main/java/com/ivo/dev/boox/rnd/screen/RandomizeService.java
+++ app/src/main/java/com/ivo/dev/boox/rnd/screen/RandomizeService.java
@@ -1,10 +1,15 @@
 package com.ivo.dev.boox.rnd.screen;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Build;
 import android.os.IBinder;
 import android.widget.Toast;
 import androidx.annotation.Nullable;
@@ -12,6 +17,9 @@ import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkManager;
 
 public class RandomizeService extends Service {
+
+    public static final String CHANNEL_ID = "RandomizeServiceChannel";
+    public static final int ONGOING_NOTIFICATION_ID = 123;
 
     BroadcastReceiver broadcastReceiver;
 
@@ -31,7 +39,27 @@ public class RandomizeService extends Service {
             }
         };
         registerReceiver(broadcastReceiver, new IntentFilter("android.intent.action.USER_PRESENT"));
+
+        createNotificationChannel();
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        Notification notification =
+                new Notification.Builder(this, CHANNEL_ID)
+                        .setContentTitle("Boox Random Screensaver")
+                        .setContentText("Service running")
+                        .setSmallIcon(R.drawable.ic_launcher_foreground)
+                        .setContentIntent(pendingIntent)
+                        .build();
+
+        startForeground(ONGOING_NOTIFICATION_ID, notification);
         Toast.makeText(getApplicationContext(), "Service started", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
     }
 
     @Override
@@ -39,6 +67,20 @@ public class RandomizeService extends Service {
         if (broadcastReceiver != null) {
             unregisterReceiver(broadcastReceiver);
             Toast.makeText(getApplicationContext(), "Service stopped", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel serviceChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Randomize Service Channel",
+                    NotificationManager.IMPORTANCE_MIN
+            );
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            if (manager != null) {
+                manager.createNotificationChannel(serviceChannel);
+            }
         }
     }
 }
